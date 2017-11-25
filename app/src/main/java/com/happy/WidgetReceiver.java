ddbfff12ff95c87bbde5e77ba50188777ec8146c
Tal <you@example.com>
2017-11-26 00:24:22 +0200

    supporting listview now with onItemClick for the container and onClick for each item, added multiple widgets support

---
+++ app/src/main/java/com/happy/WidgetReceiver.java
@@ -0,0 +1,33 @@
+package com.happy;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Tal on 25/11/2017.
+ */
+
+public class WidgetReceiver extends AppWidgetProvider
+{
+    Intent lastIntent;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        lastIntent = intent;
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+
+        Log.d("HAPY", "onUpdate");
+        Intent serviceIntent = new Intent(context, Widget.class);
+        serviceIntent.putExtra(Widget.WIDGET_INTENT, lastIntent);
+        context.startService(serviceIntent);
+    }
+}
