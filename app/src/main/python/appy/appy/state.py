1e468902d9dcf82386bbab5480fc23488939687a
Tal <you@example.com>
2018-09-20 01:35:56 +0300

    added bracket notation to state

--- app/src/main/python/appy/appy/state.py
+++ app/src/main/python/appy/appy/state.py
@@ -76,7 +76,16 @@ class State:
 
     def __delattr__(self, attr):
         self.__changeattr__(attr, delete=True)
-
+       
+    def __getitem__(self, key):
+        return self.__getattr__(key)
+        
+    def __setitem__(self, key, value):
+        return self.__setattr__(key, value)
+        
+    def __delitem__(self, key):
+        return self.__delattr__(key)
+        
     def __dir__(self):
         return list(
                         set(self.__getscope__('locals', self.__info__.scope_keys.locals).keys()) |
