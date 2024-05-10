a7d867414adde81a371abf40b29626c9d932954d
Tal <you@example.com>
2024-05-11 02:20:23 +0300

    Added bottom text when location permission is not given

--- app/src/main/java/com/appy/MainActivity.java
+++ app/src/main/java/com/appy/MainActivity.java
@@ -7,11 +7,8 @@ import android.content.DialogInterface;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.os.Build;
-import android.os.Handler;
 import android.os.IBinder;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -19,32 +16,20 @@ import androidx.core.view.GravityCompat;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
-
-import com.google.android.material.internal.ManufacturerUtils;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.fragment.app.FragmentManager;
 import androidx.appcompat.app.ActionBar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import androidx.appcompat.widget.Toolbar;
-
 import android.util.Log;
 import android.util.Pair;
-import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements StatusListener
@@ -55,6 +40,7 @@ public class MainActivity extends AppCompatActivity implements StatusListener
     private DrawerLayout drawer;
     private Toolbar toolbar;
     private NavigationView navView;
+    private TextView bottomText;
     private HashMap<Integer, Pair<Class<?>, Fragment>> fragments = new HashMap<>();
     public static final String FRAGMENT_TAG = "FRAGMENT";
 
@@ -102,6 +88,8 @@ public class MainActivity extends AppCompatActivity implements StatusListener
                     }
                 });
 
+        bottomText = findViewById(R.id.bottomtext);
+        bottomText.setVisibility(View.GONE);
 
         getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener()
         {
@@ -169,7 +157,12 @@ public class MainActivity extends AppCompatActivity implements StatusListener
     {
         if (!granted)
         {
-            Toast.makeText(this, "All time location access denied, appy might have trouble running (because android kills it)", Toast.LENGTH_LONG).show();
+            bottomText.setVisibility(View.VISIBLE);
+            //Toast.makeText(this, "All time location access denied, appy might have trouble running (because android kills it)", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            bottomText.setVisibility(View.GONE);
         }
     }
 
@@ -243,7 +236,8 @@ public class MainActivity extends AppCompatActivity implements StatusListener
                     public void onClick(DialogInterface dialog, int which)
                     {
                         setShowPermissionDialog(false);
-                        ActivityCompat.requestPermissions(MainActivity.this, permissionsSteps[neededStep], REQUEST_PERMISSION_STEPS[neededStep]);
+                        permissionsResult(false);
+                        //ActivityCompat.requestPermissions(MainActivity.this, permissionsSteps[neededStep], REQUEST_PERMISSION_STEPS[neededStep]);
                     }
                 });
 
@@ -265,6 +259,12 @@ public class MainActivity extends AppCompatActivity implements StatusListener
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
     {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (permissions.length == 0)
+        {
+            //what?
+            return;
+        }
 
         boolean allGranted = true;
         for (int granted : grantResults)
@@ -467,6 +467,12 @@ public class MainActivity extends AppCompatActivity implements StatusListener
         if (tutorial != null)
         {
             tutorial.onActivityResumed();
+        }
+        if (tutorial == null || tutorial.isFinished())
+        {
+            Log.d("APPY", "you?");
+            int neededStep = checkPermissions();
+            permissionsResult(neededStep == -1);
         }
     }
 }
